8a5613de6a68be0a80ea173956c5f1259b8630cc
abdel <karmouni-abdelali@outlook.fr>
2025-06-25 15:56:27 +0200

    Initial commit - Symfony project

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,41 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class LuckyController
+{
+    #[Route('/lucky/number')]
+    
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+
+
+
+     #[Route('/send-email')]
+
+     public function sendEmail(MailerInterface $mailer): Response
+{
+    $email = (new Email())
+        ->from('from@example.com')
+        ->to('to@example.com')
+        ->subject('Test Email Symfony')
+        ->text('Voici un test d\'envoi de mail via SMTP !')
+        ->html('<p>Voici un test d\'<strong>envoi de mail</strong> via SMTP !</p>');
+
+    $mailer->send($email);
+
+    return new Response('Email envoyé !');
+}
+}
